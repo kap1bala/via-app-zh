19e733bf515baccd0e75f86ac4082287c26f8e9b
kap1bala <243586394+kap1bala@users.noreply.github.com>
2025-11-14 20:36:21 +0800

    汉化 LOADING-TEXT

--- src/components/loading-text.tsx
+++ src/components/loading-text.tsx
@@ -6,8 +6,8 @@ const LoadingText = styled.div`
 `;
 
 enum LoadingLabel {
-  Searching = 'Searching for devices...',
-  Loading = 'Loading...',
+  Searching = '搜索设备中...',
+  Loading = '加载中...',
 }
 
 type Props = {
